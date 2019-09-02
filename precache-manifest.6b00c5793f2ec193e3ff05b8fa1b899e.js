a776490b66a053e851360ff3adce8e2d4cdb0589
John Laury <johnlaury_p@hotmail.com>
2019-09-02 00:47:54 -0700

    Updates

---
+++ precache-manifest.6b00c5793f2ec193e3ff05b8fa1b899e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1db10f8a88d22b1e48107095edf4184",
+    "url": "/johnlauryGithub/etooltest3hr/index.html"
+  },
+  {
+    "revision": "e5222eee97eeea512d74",
+    "url": "/johnlauryGithub/etooltest3hr/static/css/main.a7c61478.chunk.css"
+  },
+  {
+    "revision": "4f1b1da8354d4d0109f2",
+    "url": "/johnlauryGithub/etooltest3hr/static/js/2.a8ba5745.chunk.js"
+  },
+  {
+    "revision": "e5222eee97eeea512d74",
+    "url": "/johnlauryGithub/etooltest3hr/static/js/main.6b615021.chunk.js"
+  },
+  {
+    "revision": "75082405604b1d355da4",
+    "url": "/johnlauryGithub/etooltest3hr/static/js/runtime~main.a015f5a1.js"
+  }
+]);
